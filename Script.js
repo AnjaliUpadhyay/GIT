f490b1106e33e224b912596a76d3b369e6f23c65
Anjali Upadhyay <u.upadhyayanjali@gmail.com>
2018-07-20 10:28:56 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(document).ready(function () {
+	$("[type = submit]").click(function () {
+		var name    = $("#name").val();
+		var comment = $("#Comment").val();
+		$("<p>"+ "<img src='Blank_Avatar.png' style='margin-right:5px;'>" +"<b>" +name+"</b>" + " commented: " + "<br>" +comment + "</p>").appendTo("div");
+	});
+});
